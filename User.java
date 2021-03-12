214f46bff0d90eb36349e065a873bee0446eea97
MaxwellIyama <bigmaxxx34@gmail.com>
2021-03-11 19:48:43 -0500

    Adding Users

---
+++ User.java
@@ -0,0 +1,20 @@
+public class User {
+    
+    private String name;
+    private String title;
+    private String race;
+    private String dob;
+    private String address;
+    private int password;
+    private String username;
+
+    public User(String name, String title, String race, String dob, String address, int password, String username) {
+        this.name = name;
+        this.title = title;
+        this.race = race;
+        this.dob = dob;
+        this.address = address;
+        this.password = password;
+        this.username = username;
+    }
+}
