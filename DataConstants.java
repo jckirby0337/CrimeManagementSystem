1ae2ad643f8fba393abd5324fce2f009fdd0a84d
Jackson Kirby <jacksonckirby@gmail.com>
2021-03-12 19:43:06 -0500

    Adding the data constants for the JSON files

---
+++ DataConstants.java
@@ -0,0 +1,65 @@
+public abstract class DataConstants {
+
+// Crime Fields    
+    protected static final String CRIME_FILE_NAME = "CrimeManagementSystem/Crime.json";
+    protected static final String CRIME_ID = "Crime id";
+    protected static final String OFFENDER_ID = "offender id";
+    protected static final String CRIME_COMMITTED = "crime committed";
+    protected static final String CRIME_LOCATION = "crime location";
+    protected static final String CRIME_DATE = "crime date";
+    protected static final String CRIMINAL = "Captain Price";
+    protected static final String CRIMINAL_CUSTODY = "criminal custody";
+    protected static final String EVIDENCE = "evidence";
+    protected static final String SOLVED = "solved";
+
+// Suspect Fields    
+    protected static final String SUSPECT_FILE_NAME = "CrimeManagementSystem/Suspect.json";
+    protected static final String SUSPECT_CRIME_ID_ = "CrimeID"; //crime id?
+    protected static final String SUSPECT_ID = "SuspectID";
+    protected static final String SUSPECT_VICTIM_ID = "VictimID";
+    protected static final String SUSPECT_NAME = "Name";
+    protected static final String SUSPECT_AGE = "Age";
+    protected static final String SUSPECT_SEX = "Sex";
+    protected static final String SUSPECT_RACE = "Race";
+    protected static final String SUSPECT_TATTOOS = "Tattoos";
+    protected static final String SUSPECT_VEHICLE = "Vehicle";
+    protected static final String SUSPECT_LICENSE_PLATE = "License-Plate";
+    protected static final String SUSPECT_ADDRESS = "Address";
+    protected static final String SUSPECT_BANK_ACCOUNT = "Bank-Account";
+    protected static final String SUSPECT_CREDIT_CARD = "Credit-Card";
+    protected static final String SUSPECT_ARMED = "Armed";
+    protected static final String SUSPECT_PUBLIC_RISK = "Public-Risk";
+    protected static final String SUSPECT_NICKNAMES = "Nicknames";
+    protected static final String SUSPECT_MENTAL_STATE = "Mental-State";
+    protected static final String SUSPECT_HOUSE_LOCATION = "House-Location";
+    protected static final String SUSPECT_EDUCATION_LEVEL = "Education-Level";
+
+// Offender Fields
+    protected static final String OFFENDER_FILE_NAME = "CrimeManagementSystem/Offender.json";
+    protected static final String OFFENDER_OFFENDER_ID = "id";
+    protected static final String OFFENDER_VICTIM_ID = "victimID";
+    protected static final String OFFENDER_TATTOOS = "Tattoos";
+    protected static final String OFFENDER_VEHICLE = "Vehicle";
+    protected static final String OFFENDER_LICENSE_PLATE = "licensePlate";
+    protected static final String OFFENDER_CRIME_LEVEL = "crimeLevels";
+    protected static final String OFFENDER_ADDRESS = "Address";
+    protected static final String OFFENDER_BANK_ACCOUNT = "bankAccount";
+    protected static final String OFFENDER_CREDIT_CARD = "creditCard";
+    protected static final String OFFENDER_ARMED = "armed";
+    protected static final String OFFENDER_PUBLIC_RISK = "publicRisk";
+    protected static final String OFFENDER_NICKNAMES = "nicknames";
+    protected static final String OFFENDER_MENTAL_STATE = "mentalState";
+    protected static final String OFFENDER_HOUSING_LOCATIONS = "housingLocations";
+    protected static final String OFFENDER_EDUCATION_LEVEL = "educationLevel";
+    
+// User Fields 
+    protected static final String USER_FILE_NAME = "CrimeManagementSystem/user.json";
+    protected static final String USER_USER_ID = "UserID";
+    protected static final String USER_NAME = "name";
+    protected static final String USER_TITLE = "title";
+    protected static final String USER_RACE = "Race";
+    protected static final String USER_DATE_OF_BIRTH = "DOB";
+    protected static final String USER_ADDRESS = "Address";
+    protected static final String USER_USER_PASSWORD = "UserPW";
+    protected static final String USER_USERNAME = "Username";
+}
