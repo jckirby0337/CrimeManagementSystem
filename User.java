594da41337080cad3da579e5491b0b3c17816cae
Jackson Kirby <jacksonckirby@gmail.com>
2021-03-14 15:15:02 -0400

    Added getters for the User class

--- User.java
+++ User.java
@@ -2,16 +2,18 @@ package CrimeManagementSystem;
 import java.util.ArrayList;
 public class User {
     
+    private String userID;
     private String name;
     private String title;
     private String race;
     private String dob;
     private String address;
-    private int password;
+    private String password;
     private String username;
     private ArrayList<User> users;
 
-    public User(String name, String title, String race, String dob, String address, int password, String username) {
+    public User(String userID, String name, String title, String race, String dob, String address, String password, String username) {
+        this.userID = userID;
         this.name = name;
         this.title = title;
         this.race = race;
@@ -20,7 +22,15 @@ public class User {
         this.password = password;
         this.username = username;
     }
-
+    public String getUserID() {
+        return userID;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
     public void addUser(User user) {
         if(users.contains(user)) {
             System.out.print("Error User Already Exist");
