71ca6f2d3441b34c4415b431436906769ed03140
brianmason2001 <77559083+brianmason2001@users.noreply.github.com>
2021-03-15 12:15:48 -0400

    adjusting

--- DataLoader.java
+++ DataLoader.java
@@ -38,4 +38,38 @@ public class DataLoader extends DataConstants{
 		
 		return null;
 	}
+
+	public static ArrayList<Suspect> getSuspects() {
+		ArrayList<Suspect> suspects = new ArrayList<Suspect>();
+		
+		try {
+			FileReader reader = new FileReader(SUSPECT_FILE_NAME);
+			JSONParser parser = new JSONParser();	
+			JSONArray suspectsJSON = (JSONArray)new JSONParser().parse(reader);
+			
+			for(int i=0; i < suspectsJSON.size(); i++) {
+				JSONObject suspectJSON = (JSONObject)suspectsJSON.get(i);
+				int crimeID = ((Long)suspectJSON.get(SUSPECT_CRIME_ID)).intValue();
+                int suspectID = ((Long)suspectJSON.get(OFFENDER_ID)).intValue();
+				int victimID = ((Long)suspectJSON.get(OFFENDER_ID)).intValue();
+				String suspectName = (String)suspectJSON.get(CRIME_COMMITTED);
+				int name = ((Long)suspectJSON.get(OFFENDER_ID)).intValue();
+				String crimeLocation = (String)suspectJSON.get(CRIME_LOCATION);
+				String crimeDate = (String)suspectJSON.get(CRIME_DATE);
+                String criminal = (String)suspectJSON.get(CRIMINAL);
+                boolean criminalCustody = (boolean)suspectJSON.get(CRIMINAL_CUSTODY);
+                String evidence = (String)suspectJSON.get(EVIDENCE);
+                boolean solved = (boolean)suspectJSON.get(SOLVED);
+				
+				crimes.add(new Crime(crimeID, offenderID, crimeCommitted, crimeLocation, crimeLocation, crimeDate, criminal, criminalCustody, evidence, solved));
+			}
+			
+			return crimes;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
 }
