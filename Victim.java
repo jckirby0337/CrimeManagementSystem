0a0e689f204366f65d5b4bb3b61a288da3056e4b
brianmason2001 <77559083+brianmason2001@users.noreply.github.com>
2021-03-18 21:28:33 -0400

    added an overloaded constructor to victim and added UUID for victim

--- Victim.java
+++ Victim.java
@@ -1,10 +1,24 @@
+import java.util.UUID;
 public class Victim extends Details{
+    private UUID victimID;
     private String details;
 
     public Victim(String name, int age, char sex, String race, String details) {
         super(name, age, sex, race);
+        this.victimID = UUID.randomUUID();
         this.details = details;
     }
+
+    public Victim(UUID victimID, String name, int age, char sex, String race, String details) {
+        super(name, age, sex, race);
+        this.victimID = victimID;
+        this.details = details;
+    }
+
+    public UUID getVictimID() {
+        return victimID;
+    }
+
     public String getDetails() {
         return this.details;
     }
@@ -12,6 +26,4 @@ public class Victim extends Details{
     public void setDetails(String details) {
         this.details = details;
     }
-
-
 }
