e076f3ace4e7a8e3766a3ab78b46ebc8eb9f29c5
brianmason2001 <77559083+brianmason2001@users.noreply.github.com>
2021-03-22 17:34:17 -0400

    Reverted crime management system

--- CrimeManagementSystem.java
+++ CrimeManagementSystem.java
@@ -6,40 +6,23 @@ public class CrimeManagementSystem {
     private Victims victims;
     private Witnesses witnesses;
     private User currentUser;
+    private UUID currentCrimeID;
+    private UUID currentSuspectID;
+    private UUID currentVictimID;
+    private UUID currentWitnessID;
+
+
 
     public CrimeManagementSystem() {
         crimes = Crimes.getInstance();
         suspects = Suspects.getInstance();
         users = Users.getInstance();
         victims = Victims.getInstance();
+        witnesses = Witnesses.getInstance();
     }
 
     public boolean createAccount(String name, String title, String race, String dob, String address, String password, String username) {
         return users.addUser(username, title, race, dob, address, password, username);
-    }
-
-    public boolean createCrime(int caseNum, UUID crimeID, UUID suspectID, UUID victimID, UUID witnessID, String crimeCommited, String crimeLocation, 
-                            String crimeDate, String criminal, boolean criminalInCustody, String evidence, boolean isSolved) {
-        
-        return crimes.addCrime(caseNum, crimeID, suspectID, victimID, witnessID, crimeCommited, crimeLocation, crimeDate, criminal, criminalInCustody, 
-        evidence, isSolved);
-    }
-
-    public boolean createSuspect(String name, int age, char sex, String race, Boolean tattoos,String vehicle, String licensePlate, String address, 
-                                Boolean bankAccount, Boolean creditCard, Boolean armed, Boolean publicRisk, String nickNames,String mentalState, 
-                                String housingLocation, String educationLevel) {
-        
-        return suspects.addSuspect(name, age, sex, race, tattoos, vehicle, licensePlate, address, bankAccount, creditCard, armed, 
-        publicRisk, nickNames, mentalState, housingLocation, educationLevel);
-    }
-
-    public boolean createVictim(String name, int age, char sex, String race, String details) {
-        return victims.addVictim(name, age, sex, race, details);
-    }
-    
-    public boolean createWitness(String name, int age, char sex, String race, String details, String story, boolean isDefending) {
-        return witnesses.addWitness(name, age, sex, race, details, story, isDefending);
-
     }
 
     public boolean login(String username, String password) {
@@ -48,7 +31,6 @@ public class CrimeManagementSystem {
         currentUser = users.getUser(username);
         return true;
     }
-    public Sus
 
     public boolean userCheck(String username, String password) {
         if(users.haveUser(username, password))return false;
@@ -59,7 +41,59 @@ public class CrimeManagementSystem {
         return currentUser;
     }
 
+    public UUID getCurrentCrime() {
+        return currentCrimeID;
+    }
+
+    public Crime getCrime(int caseNum) {
+        return crimes.getCrime(caseNum);
+    }
+
+    public boolean createCrime(int caseNum, String crimeCommited, String crimeLocation, String crimeDate, String criminal, boolean criminalInCustody, String evidence, boolean isSolved) {
+        if(crimes.addCrime(caseNum, crimeCommited, crimeLocation, crimeDate, criminal, criminalInCustody, evidence, isSolved)) {
+            currentCrimeID = crimes.getCrime(caseNum).getCrimeID();
+            return true;
+        }
+        return false;
+    }
+
+    public boolean createSuspect(boolean isConnectedToCrime, String name, int age, char sex, String race, Boolean tattoos,
+    String vehicle, String licensePlate, String address, Boolean bankAccount,
+    Boolean creditCard, Boolean armed, Boolean publicRisk, String nickNames,
+    String mentalState, String housingLocation, String educationLevel) {
+        if(suspects.addSuspect(name, age, sex, race, tattoos, vehicle, licensePlate, address, bankAccount, 
+        creditCard, armed, publicRisk, nickNames, mentalState, housingLocation, 
+        educationLevel) && isConnectedToCrime) {
+            currentSuspectID = suspects.getSuspect(name).getSuspectID();
+            return crimes.addSuspectID(currentCrimeID, currentSuspectID);
+        }
+        else if(suspects.addSuspect(name, age, sex, race, tattoos, vehicle, licensePlate, address, bankAccount, 
+        creditCard, armed, publicRisk, nickNames, mentalState, housingLocation, 
+        educationLevel)) return true;
+
+        return false;
+    }
+
+    public boolean createVictim(String name, int age, char sex, String race, String details) {
+        if(victims.addVictim(name, age, sex, race, details)) {
+            currentVictimID = victims.getVictim(name).getVictimID();
+            return crimes.addVictimID(currentCrimeID, currentVictimID);
+        }
+        return false;
+    }
+
+    public boolean createWitness(String name, int age, char sex, String race, String details, String story, boolean isDefending) {
+        if(witnesses.addWitness(name, age, sex, race, details, story, isDefending)) {
+            currentWitnessID = witnesses.getWitness(name).getWitnessID();
+            return crimes.addWitnessID(currentCrimeID, currentWitnessID);
+        }
+        return false;
+    }
+
     public void logout() {
         users.saveUsers();
+        crimes.saveCrime();
+        victims.saveVictims();
+        witnesses.saveWitnesses();
     }
 }
